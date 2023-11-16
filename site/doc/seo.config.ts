98704dd6b1310b50602349bb120bb276253bf932
DMY <147dmy@gmail.com>
2023-11-16 19:25:27 +0800

    chore: update meta

---
+++ site/doc/seo.config.ts
@@ -0,0 +1,32 @@
+import type { Metadata } from "next";
+
+const defineMetadata = <T extends Metadata>(metadata: T) => metadata;
+
+const seoConfig = defineMetadata({
+  metadataBase: new URL("https://doc.rabby.io"),
+  title: {
+    template: "%s - RabbyKit",
+    default:
+      "RabbyKit - A Real Web3 Way to connect all wallets for all Dapp developers",
+  },
+  description:
+    "RabbyKit - A Real Web3 Way to connect all wallets for all Dapp developers",
+  themeColor: "#FFF",
+  openGraph: {
+    images: "/og-image.png",
+    url: "https://doc.rabby.io",
+  },
+  manifest: "/site.webmanifest",
+  icons: [
+    { rel: "icon", url: "/favicon.png" },
+    { rel: "apple-touch-icon", url: "/favicon.png" },
+    { rel: "mask-icon", url: "/favicon.png" },
+    { rel: "image/x-icon", url: "/favicon.png" },
+  ],
+  twitter: {
+    site: "@Rabby_io",
+    creator: "@Rabby_io",
+  },
+});
+
+export default seoConfig;
