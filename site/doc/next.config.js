df3d2f075741a8388d832b4e061fbb69715955be
DMY <147dmy@gmail.com>
2023-11-22 15:39:40 +0800

    chore: next standalone output

--- site/doc/next.config.js
+++ site/doc/next.config.js
@@ -2,8 +2,9 @@ const withNextra = require("nextra")({
   theme: "nextra-theme-docs",
   themeConfig: "./theme.config.tsx",
 });
-
 module.exports = withNextra({
+  output: "standalone",
+
   async redirects() {
     return [
       {
